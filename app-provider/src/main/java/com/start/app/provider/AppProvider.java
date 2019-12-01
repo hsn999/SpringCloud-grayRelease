3efe2161c7388933263f98fe210280b66a88b938
hsn999 <h_sn999@126.com>
2019-12-02 00:00:58 +0800

    update

--- app-provider/src/main/java/com/start/app/provider/AppProvider.java
+++ app-provider/src/main/java/com/start/app/provider/AppProvider.java
@@ -1,4 +1,4 @@
-package com.start.provider;
+package com.start.app.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- app-provider/src/main/java/com/start/app/provider/controller/AppServiceController.java
+++ app-provider/src/main/java/com/start/app/provider/controller/AppServiceController.java
@@ -1,4 +1,4 @@
-package com.start.provider.controller;
+package com.start.app.provider.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
